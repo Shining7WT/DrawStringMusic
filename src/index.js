c78db47ecac2834f8c9825ed190925a5f6eb55d5
Zhang <94394783+Shining7WT@users.noreply.github.com>
2024-11-13 02:07:21 +0800

    done

--- src/index.js
+++ src/index.js
@@ -17,5 +17,5 @@ ReactDOM.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// sent all
+// Done
 reportWebVitals();
